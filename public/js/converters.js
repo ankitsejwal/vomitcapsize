f0c18da4d2512088e63018d1faa3843eaaf56a0e
ankitsejwal <ankit@sejw.al>
2020-11-06 02:44:26 +1100

    add font + - feature

--- public/js/converters.js
+++ public/js/converters.js
@@ -1,3 +1,17 @@
+// font size
+function fontSize(value) {
+  let paragraph = document.getElementById("paragraph");
+  let style = window
+    .getComputedStyle(paragraph, null)
+    .getPropertyValue("font-size");
+  var fontSize = parseFloat(style);
+
+  console.log(fontSize);
+  fontSize = fontSize + value + "px";
+  paragraph.style.fontSize = fontSize;
+  paragraph.style.lineHeight = fontSize;
+}
+
 // convert paragraph
 
 function uppercase() {
