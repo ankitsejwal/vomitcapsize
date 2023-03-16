3c315b4489fb4fc41d96bb6d7ae7a823f10b0c6b
ankitsejwal <ankit@sejw.al>
2023-03-16 21:00:58 +0530

    implemented most of the functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,32 +5,63 @@ export default function TextForm() {
 
   const upperCase = () => setText(text.toUpperCase());
   const lowerCase = () => setText(text.toLowerCase());
+  const capitalizeCase = () => {
+    let words = text.trim().split(" ");
+    let capitalizedWords = [];
+
+    for (let w of words) {
+      capitalizedWords.push(w.replace(w[0], w[0].toUpperCase()));
+    }
+    words = capitalizedWords.join(" ");
+    setText(words);
+  };
+  const clearText = () => setText("");
 
   const handleOnChange = (event) => {
-    console.log("key pressed");
     setText(event.target.value);
   };
 
   return (
     <>
-      <div className="my-5 p-5">
+      <div className="container mt-5 px-5">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">
           Paste/Type your text here:
         </label>
         <textarea
-          className="form-control"
+          className="form-control my-3"
           id="textBox"
           rows="8"
           value={text}
           onChange={handleOnChange}
         ></textarea>
+        <button className="btn btn-primary mx-1" onClick={upperCase}>
+          UPPERCASE
+        </button>
+        <button className="btn btn-secondary mx-1" onClick={lowerCase}>
+          lowercase
+        </button>
+        <button className="btn btn-info mx-1" onClick={capitalizeCase}>
+          Capitalize
+        </button>
+        <button className="btn btn-danger mx-1" onClick={clearText}>
+          clear
+        </button>
       </div>
-      <button className="btn btn-primary mx-1" onClick={upperCase}>
-        UPPERCASE
-      </button>
-      <button className="btn btn-danger mx-1" onClick={lowerCase}>
-        lowercase
-      </button>
+      <div className="container p-5">
+        <h3>Summary</h3>
+        <p>
+          characters: <strong>{text.length}</strong> <br />
+          words: <b>{text.split(" ").length}</b>
+        </p>
+        <p>
+          Reading time <b>{Math.round(0.008 * text.split(" ").length)}</b>{" "}
+          minutes
+        </p>
+        <div className="contianer mt-5">
+          <h3>Preview</h3>
+          <p>{text}</p>
+        </div>
+      </div>
     </>
   );
 }
